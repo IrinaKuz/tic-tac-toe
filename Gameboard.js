e307b7d5a5d74c1444b469968453d574a11fef0a
IrinaKuz <ira.kuznets@gmail.com>
2021-02-02 21:21:14 -0500

    Added computer's play

--- Gameboard.js
+++ Gameboard.js
@@ -1,8 +1,8 @@
 const Gameboard = (() => {
-    let cols = 3;
+    const cols = 3;
     let cells = ['', '', '', '', '', '', '', '', ''];
     let turn = 'X'; // X or O
-    let status = 'active';
+    let isPlay = true;
     // methods
     const recordTurn = (ind) => {
         cells[ind] = turn;
@@ -30,7 +30,7 @@ const Gameboard = (() => {
         return true;
     };
     const checkWin = () => {
-        const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]];
+        const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
         for (let i = 0; i < winCombos.length; i++) {
             if (cells[winCombos[i][0]] === cells[winCombos[i][1]] && cells[winCombos[i][0]] === cells[winCombos[i][2]] && cells[winCombos[i][0]] != '') {
                 return true;
@@ -44,23 +44,47 @@ const Gameboard = (() => {
             cellsElements[i].removeEventListener('click', clickListener);
         }
     };
+
     const displayInfo = (text) => {
-        document.querySelector('#info').innerText = text;
-        document.querySelector('#info').style.display = 'flex';
+        const info = document.querySelector('#info');
+        info.innerText = text;
+        info.style.display = 'flex';
+        const btn = document.createElement('a');
+        btn.setAttribute('href', '/index.html');
+        btn.setAttribute('class', 'playAgain');
+        btn.innerText = 'Play Again?';
+        info.appendChild(btn);
     };
+    const makeTurn = (ind) => {
+        if(checkCell(ind)) {
+            recordTurn(ind);
+            draw();
+            if (checkWin()) {
+                displayInfo("Player " + turn + " WON!");
+                removeListeners();
+                isPlay = false;
+            } else if(checkTie()) {
+                displayInfo("It's a TIE!");
+                removeListeners();
+                isPlay = false;
+            }
+            if(isPlay) {
+                changeTurn();
+            }
+        }
+    }
+    const findEmptyCell = () => {
+        let random = Math.floor(Math.random() * 9);
+        while(cells[random] != '') {
+            random = Math.floor(Math.random() * 9);
+        }
+        return random;
+    }
     const clickListener = (e) => {
             let ind = e.target.getAttribute('data-id');
-            if(checkCell(ind)) {
-                recordTurn(ind);
-                draw();
-                if(checkTie()) {
-                    displayInfo("It's a TIE!");
-                    removeListeners();
-                } else if (checkWin()) {
-                    displayInfo("Player " + turn + " WON!");
-                    removeListeners();
-                }
-                changeTurn();
+            makeTurn(ind);
+            if(isPlay) {
+                makeTurn(findEmptyCell());
             }
     }
     const cellsAddEvents = () => {
@@ -88,5 +112,5 @@ const Gameboard = (() => {
         document.querySelector('#main').appendChild(table);
         cellsAddEvents();
     };
-    return {recordTurn, changeTurn, draw};
+    return {recordTurn, makeTurn, changeTurn, draw};
 })();
